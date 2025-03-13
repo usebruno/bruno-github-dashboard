c2fdc2be30f12a9a535a49fdc613254dbb7440c8
Anoop M D <anoop.md1421@gmail.com>
2025-03-14 01:48:37 +0530

    style: enhance navigation item styles for better UX

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
       <div className={`${geistSans.variable} ${geistMono.variable} min-h-screen min-w-[768px] font-[family-name:var(--font-geist-sans)] flex flex-col`}>
         <GithubProvider>
           <Navbar />
-          <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-6 flex-grow">
+          <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-2 flex-grow">
             <Component {...pageProps} />
           </main>
           <Footer />
--- src/pages/issues/index.js
+++ src/pages/issues/index.js
@@ -25,16 +25,19 @@ export default function Issues() {
       <nav className="mb-8">
         <div className="max-w-2xl">
           <div className="flex space-x-4">
-            {navItems.map((item, index) => (
+            {navItems.map((item) => (
               <Link
                 key={item.path}
                 href={item.path}
                 className={`
-                  text-sm
-                  ${index === 0 ? 'pl-0' : 'px-1'}
+                  px-1 py-2 text-sm font-medium transition-colors relative
                   ${tab === item.path.split('=')[1]
-                    ? 'text-amber-600'
-                    : 'text-gray-500 hover:text-gray-700'
+                    ? 'text-yellow-600 font-semibold'
+                    : 'text-gray-500 hover:text-gray-800'
+                  }
+                  ${tab === item.path.split('=')[1] 
+                    ? 'after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-yellow-600' 
+                    : 'after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-transparent hover:after:bg-gray-200'
                   }
                 `}
               >
